203b7c30818b79fdb71b40dc8d4ed6ec732bce26
slikemebro <74789521+slikemebro@users.noreply.github.com>
2023-07-22 20:52:04 +0300

    Update config.py

--- AcceptApplications/config.py
+++ AcceptApplications/config.py
@@ -1,10 +1,11 @@
-from environs import Env
+# from environs import Env
+#
+# env = Env()
+# env.read_env()
 
-env = Env()
-env.read_env()
-
-API_BOT = env.str("API_BOT")
+# API_BOT = env.str("API_BOT")
+API_BOT = '5991149626:AAGd7DYj7DoPC3khHjnLY72rpigTD0r1R2I'
 
 # LOG_CHANNEL = env.str("LOG_CHANNEL")
 
-ADMIN = env.str("ADMIN")
+# ADMIN = env.str("ADMIN")
