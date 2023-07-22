439587f8d996dd2fd4b4227848b399682f8ed69f
slikemebro <74789521+slikemebro@users.noreply.github.com>
2023-07-22 19:49:18 +0300

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,8 +1,8 @@
 import subprocess
 import logging
-from AcceptApplications.config import *
-from aiogram import Dispatcher, Bot
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+# from AcceptApplications.config import *
+# from aiogram import Dispatcher, Bot
+# from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 subprocess.Popen('python AcceptApplications/bot.py')
 
